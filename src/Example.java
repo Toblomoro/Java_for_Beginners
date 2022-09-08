c8ddbe5020cdf393461facdfa6cf43639920c4d4
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-09-08 17:23:16 -0500

    practicing from book

---
+++ src/Example.java
@@ -0,0 +1,11 @@
+public class Example {
+    public static void main(String[] args) {
+        int var1; //this declares a variable
+        int var2; // this declares a second variable
+        var1 = 1024; //this declares var1
+        System.out.println(("var1 contains " + var1));
+        var2 = var1 / 2; //this declares var2
+        System.out.println("var2 contains  var1 divided by 2");
+        System.out.println((var2));
+    }
+}
