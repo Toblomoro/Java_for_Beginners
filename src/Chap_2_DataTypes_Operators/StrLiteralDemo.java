cd26e5649e93db7afa84216defc6fd518531e095
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-09-11 12:18:51 -0500

    string literals literally completed

--- src/Chap_2_DataTypes_Operators/StrLiteralDemo.java
+++ src/Chap_2_DataTypes_Operators/StrLiteralDemo.java
@@ -5,6 +5,6 @@ public class StrLiteralDemo {
         //demonstrate use of string literals
         System.out.println("First line \nSecond line");//Use \n to start a new line, yu can use this for whoel paragraphs so you dont have to use multiple system.out.println()'s
         System.out.println("A\tB\tC");
-        System.out.println("D\tE\n\tF");//Use tabs to align char's.
+        System.out.println("D\tE\tF");//Use tabs to align char's.
     }
 }
