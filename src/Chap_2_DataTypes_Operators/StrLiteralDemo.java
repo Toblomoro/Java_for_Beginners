7fa04856a19112b105e4aa71232b28edc17640c8
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-09-11 12:18:08 -0500

    completed practice timing lightning

---
+++ src/Chap_2_DataTypes_Operators/StrLiteralDemo.java
@@ -0,0 +1,10 @@
+package Chap_2_DataTypes_Operators;
+
+public class StrLiteralDemo {
+    public static void main(String[] args) {
+        //demonstrate use of string literals
+        System.out.println("First line \nSecond line");//Use \n to start a new line, yu can use this for whoel paragraphs so you dont have to use multiple system.out.println()'s
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\n\tF");//Use tabs to align char's.
+    }
+}
